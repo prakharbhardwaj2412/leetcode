29e74b5d4dd6efb51945334bfa09d4eea10f7eec
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-04-07 11:39:48 +0530

    Time: 7 ms (12.87%), Space: 7.6 MB (34.27%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        sort(stones.begin(), stones.end(), greater<int>());
+        
+        // for(int x:stones) cout<<x<<" ";
+        
+        while(stones.size() > 1)
+        {
+            int first_max = stones[0];
+            int sec_max = stones[1];
+                stones.erase(stones.begin()+1);
+                stones.erase(stones.begin());
+                stones.push_back(first_max-sec_max);
+            sort(stones.begin(), stones.end(), greater<int>());
+        }
+        
+        return stones[0];
+        
+    }
+};
