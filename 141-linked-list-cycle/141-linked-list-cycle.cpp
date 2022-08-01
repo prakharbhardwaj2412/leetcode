68da5a543603bfa621af40727943ce647041cd5e
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-08-01 11:16:39 +0530

    Time: 45 ms (5.36%), Space: 10.4 MB (14.45%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -6,18 +6,35 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         ListNode *p, *q;
+//         p=q=head;
+//         do
+//         {
+//             p=p!=NULL?p->next:NULL;
+//             q=q!=NULL?q->next:NULL;
+//             q=q!=NULL?q->next:NULL;
+//         } while(p && q && p!=q);
+//         return (p==q && p!=0)?true:false;
+        
+//     }
+// };
+
+
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        ListNode *p, *q;
-        p=q=head;
-        do
+    bool hasCycle(ListNode *head)
+    {
+        unordered_set<ListNode*> s;
+        while(head!=NULL)
         {
-            p=p!=NULL?p->next:NULL;
-            q=q!=NULL?q->next:NULL;
-            q=q!=NULL?q->next:NULL;
-        } while(p && q && p!=q);
-        return (p==q && p!=0)?true:false;
-        
+            if(s.find(head) != s.end())
+                return true;
+            s.insert(head);
+            head=head->next;
+        }
+        return false;
     }
 };
