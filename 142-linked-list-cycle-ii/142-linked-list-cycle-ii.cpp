a22be801ad5a909925eb6bd6e0da86bfbf9388eb
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-08-01 18:28:38 +0530

    Time: 14 ms (50.61%), Space: 7.7 MB (22.48%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -6,21 +6,57 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// USING FLOYD'S CYCLE ALGO AND WITHOUT FINDING LOOP============================
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*> s;
-        ListNode *p=head;
-        while(p!=NULL)
+        ListNode *slow_p, *fast_p;
+        slow_p=fast_p=head;
+        int count=0;
+        
+        do
         {
-            if(s.find(p) != s.end())
+            count++;
+            slow_p=slow_p!=NULL?slow_p->next:NULL;
+            fast_p=fast_p!=NULL?fast_p->next:NULL;
+            fast_p=fast_p!=NULL?fast_p->next:NULL;
+        } while(slow_p && fast_p && slow_p!=fast_p);
+        cout<<count<<endl;
+        
+        if(slow_p==fast_p && slow_p!=NULL)
+        {
+            slow_p=head;
+            // cout<<slow_p->val<<" "<<fast_p->val;
+            while(slow_p!=fast_p)
             {
-                return p;
+                slow_p=slow_p->next?slow_p->next:NULL;
+                fast_p=fast_p->next?fast_p->next:NULL;
             }
-            s.insert(p);
-            p=p->next;
+            return slow_p;
         }
-        return p;
+        return NULL;
         
     }
 };
+
+
+// USING HASHING================================================
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         unordered_set<ListNode*> s;
+//         ListNode *p=head;
+//         while(p!=NULL)
+//         {
+//             if(s.find(p) != s.end())
+//             {
+//                 return p;
+//             }
+//             s.insert(p);
+//             p=p->next;
+//         }
+//         return p;
+        
+//     }
+// };
