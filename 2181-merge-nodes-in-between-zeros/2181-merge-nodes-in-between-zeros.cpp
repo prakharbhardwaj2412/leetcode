363cf3bbbed959000dec80940f2576ffa5bf6746
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-08-02 18:36:43 +0530

    Time: 1125 ms (54.30%), Space: 256.2 MB (59.15%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -57,21 +57,6 @@ public:
         head->next->val=sum;
         head->next->next=mergeNodes(ptr);
         return head->next;
-//         if(!head->next) return nullptr;
-    
-//     //fetch sum from current 0 to next 0
-//     ListNode* ptr= head->next;
-//     int sum=0;
-//     while(ptr->val!=0) sum+= ptr->val, ptr=ptr->next;
-    
-//     //assign sum on the first node between nodes having value 0.
-//     head->next->val= sum;
-    
-//     //call and get the answer and connect the answer to next of head->next
-//     head->next->next= mergeNodes(ptr);
-    
-//     //return head->next..=> new head
-//     return head->next;
         
     }
 };
