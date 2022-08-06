4e00dabc64cd718a7b88964c281110f9d6f1402c
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-08-07 00:31:33 +0530

    Time: 52 ms (55.58%), Space: 10.7 MB (71.24%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -11,22 +11,41 @@
 //         return -1;
 //     }
 // };
-// METHOD 2
+
+// METHOD 2 ===============================
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         int arr[26];
+//         for(int i=0; i<26; i++) arr[i]=-1;
+//         for(int i=0; i<s.size(); i++)
+//         {
+//             if(arr[s[i]-'a'] == -1) arr[s[i]-'a']=i;
+//             else arr[s[i]-'a']=-2;
+//         }
+        
+//         int index=INT_MAX;
+//         for(int x:arr)
+//             if(x>=0) index=min(index, x);
+//         if(index==INT_MAX) return -1;
+//         return index;
+//     }
+// };
+
+// METHOD 3
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int arr[26];
-        for(int i=0; i<26; i++) arr[i]=-1;
-        for(int i=0; i<s.size(); i++)
-        {
-            if(arr[s[i]-'a'] == -1) arr[s[i]-'a']=i;
-            else arr[s[i]-'a']=-2;
-        }
-        
-        int index=INT_MAX;
-        for(int x:arr)
-            if(x>=0) index=min(index, x);
-        if(index==INT_MAX) return -1;
-        return index;
+        for(int i = 0; i < s.length(); i++)
+       {
+
+           if (s.find(s[i] ,s.find(s[i])+1) == string::npos)
+           {
+               return i;
+
+               break;
+           }
+       }
+       return -1;
     }
 };
