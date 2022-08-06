00e369c74cf5ed737063d79df654d8a9f9d5eb2e
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-08-06 16:37:39 +0530

    Time: 294 ms (23.99%), Space: 57.3 MB (68.83%) - LeetHub

--- 933-number-of-recent-calls/933-number-of-recent-calls.cpp
+++ 933-number-of-recent-calls/933-number-of-recent-calls.cpp
@@ -2,17 +2,10 @@ class RecentCounter {
 public:
     queue<int> Q;
     RecentCounter() {
-        
+        return;
     }
     
     int ping(int t) {
-        // arr.push_back(t);
-        // int count=0;
-        // for(int x:arr)
-        // {
-        //     if(x>=t-3000 && x<=t) count++;
-        // }
-        // return count;
         Q.push(t);
         while(Q.front()<t-3000) Q.pop();
         return Q.size();
