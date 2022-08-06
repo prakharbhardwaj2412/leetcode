fb95c3be68609e8cf93a65eb8f4058f7bcab8f23
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-08-07 00:28:27 +0530

    Time: 25 ms (91.16%), Space: 10.6 MB (91.21%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,12 +1,32 @@
+// // METHOD 1
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         int arr[26]={0};
+        
+//         for(char x:s)
+//             arr[x-'a']++;
+//         for(int i=0; i<s.size(); i++)
+//             if(arr[s[i]-'a']==1) return i;
+//         return -1;
+//     }
+// };
+// METHOD 2
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int arr[26]={0};
-        
-        for(char x:s)
-            arr[x-'a']++;
+        int arr[26];
+        for(int i=0; i<26; i++) arr[i]=-1;
         for(int i=0; i<s.size(); i++)
-            if(arr[s[i]-'a']==1) return i;
-        return -1;
+        {
+            if(arr[s[i]-'a'] == -1) arr[s[i]-'a']=i;
+            else arr[s[i]-'a']=-2;
+        }
+        
+        int index=INT_MAX;
+        for(int x:arr)
+            if(x>=0) index=min(index, x);
+        if(index==INT_MAX) return -1;
+        return index;
     }
 };
