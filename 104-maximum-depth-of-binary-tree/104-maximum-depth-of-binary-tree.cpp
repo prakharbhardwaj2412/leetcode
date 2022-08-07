d66509a134359e8aae213eb71316651d2d36e830
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-08-08 01:08:31 +0530

    Time: 29 ms (5.23%), Space: 19.1 MB (14.24%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -9,13 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// RECURSIVE CALCULATION OF HEIGHT
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root) {
+//         if(!root) return 0;
+//         int x=maxDepth(root->left);
+//         int y=maxDepth(root->right);
+//         return max(x,y)+1;
+        
+//     }
+// };
+
+
+// LEVEL ORDER TRAVERSAL TO FIND HEIGHT
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(!root) return 0;
-        int x=maxDepth(root->left);
-        int y=maxDepth(root->right);
-        return max(x,y)+1;
+        queue<TreeNode*> q;
+        TreeNode* t=root;
+        int height=0;
+        q.push(t);
+        q.push(NULL);
+        while(!q.empty())
+        {
+            t=q.front();
+            q.pop();
+            if(!t) height++;
+            
+            if(t)
+            {
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            }
+            else if(!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        return height;
         
     }
 };
