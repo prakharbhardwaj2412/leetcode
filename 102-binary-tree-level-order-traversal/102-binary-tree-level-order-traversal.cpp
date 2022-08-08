5a647a09ea32987902fc4710dd3ea286db48933e
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-08-08 13:51:01 +0530

    Time: 7 ms (55.49%), Space: 12.5 MB (85.19%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -1,3 +1,56 @@
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         vector<vector<int>> res;
+//         vector<int> temp;
+//         TreeNode *t=root;
+//         queue<TreeNode *> q;
+//         if(t)
+//         {
+//             q.push(t);
+//             q.push(NULL);
+//             temp.push_back(t->val);
+//             res.push_back(temp);
+//         }
+//         temp.clear();
+//         while(!q.empty())
+//         {
+//             t=q.front();
+//             q.pop();
+//             if(!t) 
+//             {
+//                 if(temp.size()) res.push_back(temp);
+//                 temp.clear();
+//             }
+//             if(t)
+//             {
+//                 if(t->left)
+//                 {
+//                     q.push(t->left);
+//                     temp.push_back(t->left->val);
+//                 }
+//                 if(t->right)
+//                 {
+//                     q.push(t->right);
+//                     temp.push_back(t->right->val);
+//                 }
+//             }
+//             else if(!q.empty()) q.push(NULL);
+//         }
+//         return res;
+//     }
+// };
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -13,40 +66,21 @@ class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> res;
-        vector<int> temp;
-        TreeNode *t=root;
         queue<TreeNode *> q;
-        if(t)
-        {
-            q.push(t);
-            q.push(NULL);
-            temp.push_back(t->val);
-            res.push_back(temp);
-        }
-        temp.clear();
+        if(root == NULL) return res;
+        q.push(root);
         while(!q.empty())
         {
-            t=q.front();
-            q.pop();
-            if(!t) 
-            {
-                if(temp.size()) res.push_back(temp);
-                temp.clear();
+            vector<int> temp;
+            int n = q.size();
+            for(int i=0; i<n; i++){
+                auto top = q.front();
+                q.pop();
+                temp.push_back(top->val);
+                if(top->left)q.push(top->left);
+                if(top->right)q.push(top->right);
             }
-            if(t)
-            {
-                if(t->left)
-                {
-                    q.push(t->left);
-                    temp.push_back(t->left->val);
-                }
-                if(t->right)
-                {
-                    q.push(t->right);
-                    temp.push_back(t->right->val);
-                }
-            }
-            else if(!q.empty()) q.push(NULL);
+            res.push_back(temp);
         }
         return res;
     }
