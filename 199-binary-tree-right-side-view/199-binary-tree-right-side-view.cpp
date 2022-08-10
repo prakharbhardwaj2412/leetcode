e570e47f9aa22e4e08f73510ead871f2c081a2c1
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-08-10 11:22:39 +0530

    Time: 0 ms (100.00%), Space: 11.8 MB (88.40%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -9,28 +9,53 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// LEVEL ORDER ITERATIVE APPROACH
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<int> res;
+//         queue<TreeNode*> q;
+//         if(root)
+//         {
+//             q.push(root);
+//         }
+//         while(!q.empty())
+//         {
+//             int n=q.size();
+            
+//             for(int i=0; i<n; i++)
+//             {
+//                 root=q.front();
+//                 if(i==0) res.push_back(root->val);
+//                 q.pop();
+//                 if(root->right) q.push(root->right);
+//                 if(root->left)q.push(root->left);
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+// RECURSIVE LEVEL ORDER TRAVERSAL================
 class Solution {
 public:
+    void rightViewUtil(TreeNode* root, int level, int* max_level, vector<int> &res)
+    {
+        if(!root) return;
+        if(*max_level<level)
+        {
+            // cout<<root->val;
+            res.push_back(root->val);
+            *max_level=level;
+        }
+        rightViewUtil(root->right, level+1, max_level, res);
+        rightViewUtil(root->left, level+1, max_level, res);
+        
+    }
     vector<int> rightSideView(TreeNode* root) {
+        int max_level=0;
         vector<int> res;
-        queue<TreeNode*> q;
-        if(root)
-        {
-            q.push(root);
-        }
-        while(!q.empty())
-        {
-            int n=q.size();
-            
-            for(int i=0; i<n; i++)
-            {
-                root=q.front();
-                if(i==0) res.push_back(root->val);
-                q.pop();
-                if(root->right) q.push(root->right);
-                if(root->left)q.push(root->left);
-            }
-        }
+        rightViewUtil(root, 1, &max_level, res);
         return res;
     }
 };
