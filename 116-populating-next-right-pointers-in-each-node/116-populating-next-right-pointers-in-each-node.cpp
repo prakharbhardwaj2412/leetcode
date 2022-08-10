67092739961b384cc481aaf8fc0ec1a2a9f46f7e
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-08-10 13:01:25 +0530

    Time: 24 ms (78.68%), Space: 16.8 MB (87.37%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -55,10 +55,7 @@ public:
         if(!root) return NULL;
         auto L=root->left, R=root->right, N=root->next;
         if(L) L->next=R;
-        if(R) {
-            if(N) R->next=N->left;
-            else R->next=NULL;
-        }
+        if(R) if(N) R->next=N->left;
         connect(L);
         connect(R);
         return root;
