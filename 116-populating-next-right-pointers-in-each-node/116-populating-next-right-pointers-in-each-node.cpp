20815dcfd963155dfd5190e0d06d7ae12d183ac8
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-08-10 12:51:44 +0530

    Time: 27 ms (67.90%), Space: 16.7 MB (87.37%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -15,35 +15,52 @@ public:
         : val(_val), left(_left), right(_right), next(_next) {}
 };
 */
+// LEVEL ORFDER TRAVERSAL
+// class Solution {
+// public:
+//     Node* connect(Node* root) {
+//         queue<Node*> q;
+//         Node* t1=root;
+//         Node* t2;
+//         if(!t1) return NULL;
+//         q.push(t1);
+//         q.push(NULL);
+//         while(!q.empty())
+//         {
+//             int n=q.size();
+//             t1=q.front();
+//             q.pop();
+//             for(int i=0; i<n-1; i++)
+//             {
+//                 t2=q.front();
+//                 q.pop();
+//                 t1->next=t2;
+//                 if(t1)
+//                 {                    
+//                     if(t1->left) q.push(t1->left);
+//                     if(t1->right) q.push(t1->right);
+//                 }
+//                 t1=t2;
+//             }
+//             if(!q.empty()) q.push(NULL);
+//         }
+//         return root;
+//     }
+// };
 
+// DEPTH FIRST SEARCH
 class Solution {
 public:
     Node* connect(Node* root) {
-        queue<Node*> q;
-        Node* t1=root;
-        Node* t2;
-        if(!t1) return NULL;
-        q.push(t1);
-        q.push(NULL);
-        while(!q.empty())
-        {
-            int n=q.size();
-            t1=q.front();
-            q.pop();
-            for(int i=0; i<n-1; i++)
-            {
-                t2=q.front();
-                q.pop();
-                t1->next=t2;
-                if(t1)
-                {                    
-                    if(t1->left) q.push(t1->left);
-                    if(t1->right) q.push(t1->right);
-                }
-                t1=t2;
-            }
-            if(!q.empty()) q.push(NULL);
+        if(!root) return NULL;
+        auto L=root->left, R=root->right, N=root->next;
+        if(L) L->next=R;
+        if(R) {
+            if(N) R->next=N->left;
+            else R->next=NULL;
         }
+        connect(L);
+        connect(R);
         return root;
     }
 };
