88b15d1e07c40df9e255a352618a9c9b83effcb5
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-09-02 15:21:05 +0530

    Time: 10 ms (43.58%), Space: 7.4 MB (54.23%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string str="";
-        for(char x:s)
+        for(int i=0, j=s.size()-1; i<j; i++, j--)
         {
-            if(x>='a' && x<='z') str+=x;
-            else if(x>='A' && x<='Z') str+=x+32;
-            else if(isalnum(x)==8) str+=x;
-            cout<<isalnum(x)<<" "<<x<<endl;
+            while(isalnum(s[i])==0 && i<j) i++;
+            while(isalnum(s[j])==0 && i<j) j--;
+            if(tolower(s[i])!=tolower(s[j])) return false;
         }
-        // cout<<str;
-        for(int i=0, j=str.size()-1; i<j; i++, j--)
-            if(str[i]!=str[j]) return false;
         return true;
     }
 };
