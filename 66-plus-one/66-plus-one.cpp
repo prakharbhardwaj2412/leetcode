f3a4fdaf94488aa16fc4cbadf7d5e4e109aab4ae
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-09-08 10:35:58 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (61.57%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n=digits.size();
-        stack<int> st;
-        int c=1;
         for(int i=n-1; i>=0; i--)
         {
-            int t=digits[i]+c;
-            st.push(t%10);
-            c=t/10;
-            
+            if(digits[i]==9)
+            {
+                digits[i]=0;
+            }
+            else 
+            {
+                digits[i]+=1;
+                return digits;
+            }
         }
-        if(c>0) {st.push(c); n++;}
-        digits.clear();
-        for(int i=0; i<n; i++) 
-        {
-            int t=st.top();
-            digits.push_back(t);
-            st.pop();
-        }
+        digits.push_back(0);
+        digits[0]=1;
         return digits;
     }
 };
