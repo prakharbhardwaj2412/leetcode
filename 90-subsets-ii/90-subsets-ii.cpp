79ce411293a84ddd5d5266cafdcf9525945d71c2
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-09-21 01:09:19 +0530

    Time: 4 ms (68.99%), Space: 7.5 MB (89.04%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     void returnSubset(int ind, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds) {
-        if(ind==arr.size())
-        {
-            // ans.push_back(ds);
-            return;
-        }
+        ans.push_back(ds);
         for(int i=ind; i<arr.size(); i++)
         {
-            if(i>ind && arr[i]==arr[i-1]) continue;
+            if(i!=ind && arr[i]==arr[i-1]) continue;
             ds.push_back(arr[i]);
-            ans.push_back(ds);
+            // ans.push_back(ds);
             returnSubset(i+1, arr, ans, ds);
             ds.pop_back();
         }
@@ -19,10 +15,8 @@ public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> ds;
-        ans.push_back(ds);
         sort(nums.begin(), nums.end());
         returnSubset(0, nums, ans, ds);
-        // vector<vector<int>> v(ans.begin(), ans.end());
         return ans;
     }
 };
