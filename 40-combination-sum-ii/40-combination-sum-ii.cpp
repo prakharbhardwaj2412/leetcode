d1110d00fc1a21e9c821b93e8151f45a3007837d
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-09-20 19:02:21 +0530

    Time: 7 ms (77.47%), Space: 13.6 MB (19.45%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void recursionSum(int index, int target, vector<int> &temp, vector<vector<int>> &ans, vector<int> candidates) {
+        // if(index==candidates.size())
+        // {
+            if(target==0)
+            {
+                ans.push_back(temp);
+                return;
+            }
+                
+        // }
+        for(int i=index; i<candidates.size(); i++)
+        {
+            if(i>index && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target) break;
+            temp.push_back(candidates[i]);
+            recursionSum(i+1, target-candidates[i], temp, ans, candidates);
+            temp.pop_back();
+        }
+        
+        
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sort(candidates.begin(), candidates.end());
+        recursionSum(0, target, temp, ans, candidates);
+        vector<vector<int>> v(ans.begin(), ans.end());        
+        return ans;
+    }
+};
