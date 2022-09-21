c668174da99abc8c294f1ed46b7cba305d05c21a
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-09-22 02:03:19 +0530

    Time: 72 ms (18.68%), Space: 6.4 MB (51.43%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -22,14 +22,14 @@ public:
         }
         return true;
     }
-    bool isValid(vector<vector<char>> &board, int col, int row, char c)
+    bool isValid(vector<vector<char>> &board, int row, int col, char c)
     {
         for(int i=0; i<9; i++)
         {
-            if(board[col][i]==c) return false;
-            if(board[i][row]==c) return false;
+            if(board[row][i]==c) return false;
+            if(board[i][col]==c) return false;
             
-            if(board[3 * (col/3) + i/3][3 * (row/3) + i%3] == c) return false;
+            if(board[3 * (row/3) + i/3][3 * (col/3) + i%3] == c) return false;
         }
         return true;
         
