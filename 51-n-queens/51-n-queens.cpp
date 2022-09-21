732cb1935d457ab770c31a8f06219405ca04b032
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-09-22 02:17:50 +0530

    Time: 73 ms (17.53%), Space: 45.7 MB (13.93%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void solve(int col, int n, vector<string>& block, vector<vector<string>> &ans) {
+        if(col==n)
+        {
+            ans.push_back(block);
+            return;
+        }
+        for(int i=0; i<n; i++)
+        {
+            if(isValid(block, i, col, n))
+            {
+                block[i][col]='Q';
+                solve(col+1, n, block, ans);
+                block[i][col]='.';
+            }
+        }
+    }
+    bool isValid(vector<string> block, int row, int col, int n)
+    {
+        int r=row, c=col;
+        while(r>=0 && c>=0)
+        {
+            if(block[r][c]=='Q') return false;
+            r--; c--;
+        }
+        r=row; c=col;
+        while(c>=0)
+        {
+            if(block[r][c]=='Q') return false;
+            c--;
+        }
+        r=row; c=col;
+        while(c>=0 && r<n)
+        {
+            if(block[r][c]=='Q') return false;
+            c--; r++;
+        }
+        return true;
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> block;
+        string s(n, '.');
+        cout<<s;
+        for(int i=0; i<n; i++) block.push_back(s);
+        solve(0, n, block, ans);
+        return ans;
+    }
+};
