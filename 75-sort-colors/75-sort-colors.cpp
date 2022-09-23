e91bd231b6fa63a8c45686302dddd6a004d29818
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-09-23 13:41:05 +0530

    Time: 3 ms (64.63%), Space: 8.4 MB (23.60%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+//         VARIATION OF DUTCH NATIONAL FLG ALGORITHM
+//         nums[0->low-1]->0
+//         nums[high+1->n-1]->2
         int low=0, mid=0, high=nums.size()-1;
         while(mid<=high)
         {
