c7134d46c9b772828cdb328d5d5c106c6e6a40c2
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-09-27 18:45:54 +0530

    Time: 3 ms (64.12%), Space: 5.9 MB (39.28%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -23,14 +23,30 @@
 // };
 
 // ============TABULATION
+// class Solution {
+// public:
+//     int fib(int n) {
+//         vector<int> dp(n+1);
+//         if(n==0) return 0;
+//         dp[0]=0; dp[1]=1;
+//         for(int i=2; i<=n; i++)
+//             dp[i]=dp[i-1]+dp[i-2];
+//         return dp[n];
+//     }
+// };
+
+// ===============SPACE OPTIMIZATION
 class Solution {
 public:
     int fib(int n) {
-        vector<int> dp(n+1);
         if(n==0) return 0;
-        dp[0]=0; dp[1]=1;
+        int prev=1, prev2=0;
         for(int i=2; i<=n; i++)
-            dp[i]=dp[i-1]+dp[i-2];
-        return dp[n];
+        {
+            int current_i=prev+prev2;
+            prev2=prev;
+            prev=current_i;
+        }
+        return prev;
     }
 };
