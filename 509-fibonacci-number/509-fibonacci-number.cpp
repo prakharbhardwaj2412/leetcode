21af79ce24f4b44cff7d333de9afe800811bcb5c
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-09-27 18:42:52 +0530

    Time: 0 ms (100.00%), Space: 6 MB (16.50%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,11 +1,11 @@
 // ============RECURSION
-class Solution {
-public:
-    int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
-    }
-};
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n<=1) return n;
+//         return fib(n-1)+fib(n-2);
+//     }
+// };
 
 // ==========MEMOIZATION
 // class Solution {
@@ -21,3 +21,16 @@ public:
 //         return f(n, dp);
 //     }
 // };
+
+// ============TABULATION
+class Solution {
+public:
+    int fib(int n) {
+        vector<int> dp(n+1);
+        if(n==0) return 0;
+        dp[0]=0; dp[1]=1;
+        for(int i=2; i<=n; i++)
+            dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
+    }
+};
