1358e33d4e9c94bf62a10469f08ba60ce2c6f8d2
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-09-29 02:07:50 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (43.39%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    
-    int rob(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
-        int ans1, ans2;
+    int maxNonAdjacentSum(vector<int> nums)
+    {
         int prev=nums[0];
         int prev2=0;
-        for(int i=1; i<nums.size()-1; i++)
+        for(int i=1; i<nums.size(); i++)
         {
             int pick=nums[i]; 
             if(i>1) pick+=prev2;
@@ -15,20 +13,17 @@ public:
             prev2=prev;
             prev=curri;
         }
-        ans1=prev;
-        
-        prev=nums[1];
-        prev2=0;
-        for(int i=2; i<nums.size(); i++)
+        return prev;
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        vector<int> temp1, temp2;
+        for(int i=0; i<nums.size(); i++)
         {
-            int pick=nums[i]; 
-            if(i>2) pick+=prev2;
-            int notPick=0+prev;
-            int curri=max(pick, notPick);
-            prev2=prev;
-            prev=curri;
+            if(i!=0) temp1.push_back(nums[i]);
+            if(i!=n-1) temp2.push_back(nums[i]);
         }
-        ans2=prev;
-        return max(ans1, ans2);
+        return max(maxNonAdjacentSum(temp1), maxNonAdjacentSum(temp2));
     }
 };
