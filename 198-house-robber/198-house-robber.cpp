98e7aed39cb4487d693bc835edf76858e3bcb085
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-09-29 01:34:40 +0530

    Time: 4 ms (37.81%), Space: 7.7 MB (49.77%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,22 +1,37 @@
-// MEMOIZATION
-class Solution {
-public:
-    int f(int ind, vector<int> &nums, vector<int> &dp)
-    {
-        if(ind<0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int pick=nums[ind]+f(ind-2, nums, dp);
-        int notPick=0+f(ind-1, nums, dp);
-        return dp[ind]=max(pick, notPick);
-    }
-    int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> dp(n, -1);
-        f(n-1, nums, dp);
-        return dp[n-1];
-    }
-};
-// // TABULATION
+// // ==================================================SIMPLE RECURSION
+// class Solution {
+// public:
+//     int f(int ind, vector<int> &nums)
+//     {
+//         if(ind<0) return 0;
+//         int pick=nums[ind]+f(ind-2, nums);
+//         int notPick=0+f(ind-1, nums);
+//         return max(pick, notPick);
+//     }
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         return f(n-1, nums);
+//     }
+// };
+// // ===========================================================MEMOIZATION
+// class Solution {
+// public:
+//     int f(int ind, vector<int> &nums, vector<int> &dp)
+//     {
+//         if(ind<0) return 0;
+//         if(dp[ind]!=-1) return dp[ind];
+//         int pick=nums[ind]+f(ind-2, nums, dp);
+//         int notPick=0+f(ind-1, nums, dp);
+//         return dp[ind]=max(pick, notPick);
+//     }
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> dp(n, -1);
+//         f(n-1, nums, dp);
+//         return dp[n-1];
+//     }
+// };
+// // ============================================================TABULATION
 // class Solution {
 // public:
 //     int rob(vector<int>& nums) {
@@ -33,21 +48,21 @@ public:
 //         return dp[n-1];
 //     }
 // };
-// // SPACE OPTIMIZED DP
-// class Solution {
-// public:
-//     int rob(vector<int>& nums) {
-//         int prev=nums[0];
-//         int prev2=0;
-//         for(int i=1; i<nums.size(); i++)
-//         {
-//             int pick=nums[i]; 
-//             if(i>1) pick+=prev2;
-//             int notPick=0+prev;
-//             int curri=max(pick, notPick);
-//             prev2=prev;
-//             prev=curri;
-//         }
-//         return prev;
-//     }
-// };
+// // =================================================================SPACE OPTIMIZED DP
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1; i<nums.size(); i++)
+        {
+            int pick=nums[i]; 
+            if(i>1) pick+=prev2;
+            int notPick=0+prev;
+            int curri=max(pick, notPick);
+            prev2=prev;
+            prev=curri;
+        }
+        return prev;
+    }
+};
