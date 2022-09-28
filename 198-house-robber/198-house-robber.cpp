e27ffaac7d8981248061a2b2a1574af9803735ec
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-09-29 01:31:33 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (25.98%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,21 +1,39 @@
-// TABULATION
+// MEMOIZATION
 class Solution {
 public:
+    int f(int ind, vector<int> &nums, vector<int> &dp)
+    {
+        if(ind<0) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int pick=nums[ind]+f(ind-2, nums, dp);
+        int notPick=0+f(ind-1, nums, dp);
+        return dp[ind]=max(pick, notPick);
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n);
-        dp[0]=nums[0];
-        for(int i=1; i<nums.size(); i++)
-        {
-            int pick=nums[i]; 
-            if(i>1) pick+=dp[i-2];
-            int notPick=0+dp[i-1];
-            dp[i]=max(pick, notPick);
-        }
+        vector<int> dp(n, -1);
+        f(n-1, nums, dp);
         return dp[n-1];
     }
 };
-// SPACE OPTIMIZED DP
+// // TABULATION
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> dp(n);
+//         dp[0]=nums[0];
+//         for(int i=1; i<nums.size(); i++)
+//         {
+//             int pick=nums[i]; 
+//             if(i>1) pick+=dp[i-2];
+//             int notPick=0+dp[i-1];
+//             dp[i]=max(pick, notPick);
+//         }
+//         return dp[n-1];
+//     }
+// };
+// // SPACE OPTIMIZED DP
 // class Solution {
 // public:
 //     int rob(vector<int>& nums) {
