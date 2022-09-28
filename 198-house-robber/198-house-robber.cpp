5175b4e1bdf0d3834d854167dab224cc4b500b37
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-09-29 01:27:32 +0530

    Time: 6 ms (17.47%), Space: 7.8 MB (49.77%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,17 +1,35 @@
+// TABULATION
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int prev=nums[0];
-        int prev2=0;
+        int n=nums.size();
+        vector<int> dp(n);
+        dp[0]=nums[0];
         for(int i=1; i<nums.size(); i++)
         {
             int pick=nums[i]; 
-            if(i>1) pick+=prev2;
-            int notPick=0+prev;
-            int curri=max(pick, notPick);
-            prev2=prev;
-            prev=curri;
+            if(i>1) pick+=dp[i-2];
+            int notPick=0+dp[i-1];
+            dp[i]=max(pick, notPick);
         }
-        return prev;
+        return dp[n-1];
     }
 };
+// SPACE OPTIMIZED DP
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int prev=nums[0];
+//         int prev2=0;
+//         for(int i=1; i<nums.size(); i++)
+//         {
+//             int pick=nums[i]; 
+//             if(i>1) pick+=prev2;
+//             int notPick=0+prev;
+//             int curri=max(pick, notPick);
+//             prev2=prev;
+//             prev=curri;
+//         }
+//         return prev;
+//     }
+// };
