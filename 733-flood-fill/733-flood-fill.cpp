907193dcb007c55ffdcb1b9a497b31adad5284bc
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-10-03 10:29:16 +0530

    Time: 19 ms (40.63%), Space: 14.1 MB (46.86%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,9 +1,9 @@
 class Solution {
 private:
-    void dfs(int row, int col, vector<vector<int>>& image, vector<vector<bool>> &vis, int newColor, int prevColor)
+    void dfs(int row, int col, vector<vector<int>>& image, vector<vector<int>> &ans, int newColor, int prevColor)
     {
-        vis[row][col]=true;
-        image[row][col]=newColor;
+        // vis[row][col]=true;
+        ans[row][col]=newColor;
         int n=image.size();
         int m=image[0].size();
         for(int delrow=-1; delrow<=1; delrow++)
@@ -13,9 +13,9 @@ private:
                 if(delrow+delcol==-2 || delrow+delcol==0 || delrow+delcol==2) continue;
                 int nrow=row+delrow;
                 int ncol=col+delcol;
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && image[nrow][ncol]==prevColor)
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && ans[nrow][ncol]!=newColor && image[nrow][ncol]==prevColor)
                 {
-                    dfs(nrow, ncol, image, vis, newColor, prevColor);
+                    dfs(nrow, ncol, image, ans, newColor, prevColor);
                 }
             }
         }
@@ -25,8 +25,9 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n=image.size();
         int m=image[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-        dfs(sr, sc, image, vis, color, image[sr][sc]);
-        return image;
+        vector<vector<int>> ans=image;
+        // vector<vector<bool>> vis(n, vector<bool>(m, false));
+        dfs(sr, sc, image, ans, color, image[sr][sc]);
+        return ans;
     }
 };
