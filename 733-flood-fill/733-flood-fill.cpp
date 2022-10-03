8ec96864b09c46478b3e0e614de2145d02ac8adb
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-10-03 10:32:55 +0530

    Time: 8 ms (90.44%), Space: 13.9 MB (78.76%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -3,21 +3,17 @@ private:
     void dfs(int row, int col, vector<vector<int>>& image, vector<vector<int>> &ans, int newColor, int prevColor)
     {
         // vis[row][col]=true;
-        image[row][col]=newColor;
+        ans[row][col]=newColor;
         int n=image.size();
         int m=image[0].size();
-        for(int delrow=-1; delrow<=1; delrow++)
+        int delrow[]={-1, 0, +1, 0};
+        int delcol[]={0, +1, 0, -1};
+        for(int i=0; i<4; i++)
         {
-            for(int delcol=-1; delcol<=1; delcol++)
-            {
-                if(delrow+delcol==-2 || delrow+delcol==0 || delrow+delcol==2) continue;
-                int nrow=row+delrow;
-                int ncol=col+delcol;
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]!=newColor && image[nrow][ncol]==prevColor)
-                {
-                    dfs(nrow, ncol, image, ans, newColor, prevColor);
-                }
-            }
+            int nrow=row+delrow[i];
+            int ncol=col+delcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && ans[nrow][ncol]!=newColor && image[nrow][ncol]==prevColor)
+                dfs(nrow, ncol, image, ans, newColor, prevColor);
         }
         
     }
@@ -28,6 +24,6 @@ public:
         vector<vector<int>> ans=image;
         // vector<vector<bool>> vis(n, vector<bool>(m, false));
         dfs(sr, sc, image, ans, color, image[sr][sc]);
-        return image;
+        return ans;
     }
 };
