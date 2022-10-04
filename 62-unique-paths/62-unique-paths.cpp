a7f1379a4e8c225beec4d8cb1414167ec1cc466d
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-10-04 07:54:17 +0530

    Time: 3 ms (45.41%), Space: 6.6 MB (28.20%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,15 +1,44 @@
+// // // =========================RECURSION
+// // class Solution {
+// // public:
+// //     int f(int row, int col)
+// //     {
+// //         if(row==0 && col==0) return 1;
+// //         if(row<0 || col<0) return 0;
+// //         return f(row-1, col)+f(row, col-1);
+// //     }
+// //     int uniquePaths(int m, int n) {
+// //         return f(m-1, n-1);
+// //     }
+// // };
+// // =========================MEMOIZATION
+// class Solution {
+// public:
+//     int f(int row, int col, vector<vector<int>> &dp)
+//     {
+//         if(row==0 && col==0) return dp[row][col]=1;
+//         if(row<0 || col<0) return 0;
+//         if(dp[row][col]!=-1) return dp[row][col];
+//         return dp[row][col]=f(row-1, col, dp)+f(row, col-1, dp);
+//     }
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m, vector<int>(n, -1));
+//         f(m-1, n-1, dp);
+//         return dp[m-1][n-1];
+//     }
+// };
+// ===========================TABULATION
 class Solution {
 public:
-    int f(int row, int col, vector<vector<int>> &dp)
-    {
-        if(row==0 && col==0) return dp[row][col]=1;
-        if(row<0 || col<0) return 0;
-        if(dp[row][col]!=-1) return dp[row][col];
-        return dp[row][col]=f(row-1, col, dp)+f(row, col-1, dp);
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        f(m-1, n-1, dp);
+        vector<vector<int>> dp(m, vector<int>(n));
+        for(int i=0; i<m; i++) dp[i][0]=1;
+        for(int j=0; j<n; j++) dp[0][j]=1;
+        for(int i=1; i<m; i++)
+        {
+            for(int j=1; j<n; j++)
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        }
         return dp[m-1][n-1];
     }
 };
