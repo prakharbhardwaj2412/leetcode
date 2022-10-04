64512ae19d1b65460e888b6d03cc90838425e00d
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-10-04 17:51:47 +0530

    Time: 86 ms (91.10%), Space: 35.7 MB (14.08%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -4,31 +4,33 @@ private:
     {
         int n=mat.size();
         int m=mat[0].size();
-        queue<pair<pair<int, int>, int>> q;
+        queue<pair<int, int>> q;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(mat[i][j]==0) {
                     vis[i][j]=1;
                     ans[i][j]=0;
-                    q.push({{i, j}, 0});
+                    q.push({i, j});
                 }
             }
         }
         int delrow[]={-1, 0, 1, 0};
         int delcol[]={0, 1, 0, -1};
         while(!q.empty()) {
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            int d=q.front().second;
+            int row=q.front().first;
+            int col=q.front().second;
+            // int d=q.front().second;
             q.pop();
             for(int i=0; i<4; i++)
             {
                 int nrow=row+delrow[i];
                 int ncol=col+delcol[i];
                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && mat[nrow][ncol]==1) {
-                    vis[nrow][ncol]=1;
-                    ans[nrow][ncol]=d+1;
-                    q.push({{nrow, ncol}, d+1});
+                    if(ans[nrow][ncol]>ans[row][col]+1) {
+                        vis[nrow][ncol]=1;
+                        ans[nrow][ncol]=ans[row][col]+1;
+                        q.push({nrow, ncol});
+                    }
                 }
             }
         }
@@ -38,7 +40,7 @@ public:
         int n=mat.size();
         int m=mat[0].size();
         vector<vector<int>> vis(n, vector<int>(m, 0));
-        vector<vector<int>> ans(n, vector<int>(m, 0));
+        vector<vector<int>> ans(n, vector<int>(m, INT_MAX));
         bfs(mat, vis, ans);
         return ans;
     }
