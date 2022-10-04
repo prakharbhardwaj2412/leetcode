a483576756ddab759dc17b36eea82a74072eab5b
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-10-04 12:34:23 +0530

    Time: 5 ms (19.58%), Space: 6 MB (77.19%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,76 +1,93 @@
-// // // // =========================RECURSION
+// // // // // =========================RECURSION
+// // // // class Solution {
+// // // // public:
+// // // //     int f(int row, int col)
+// // // //     {
+// // // //         if(row==0 && col==0) return 1;
+// // // //         if(row<0 || col<0) return 0;
+// // // //         return f(row-1, col)+f(row, col-1);
+// // // //     }
+// // // //     int uniquePaths(int m, int n) {
+// // // //         return f(m-1, n-1);
+// // // //     }
+// // // // };
+// // // // =========================MEMOIZATION
 // // // class Solution {
 // // // public:
-// // //     int f(int row, int col)
+// // //     int f(int row, int col, vector<vector<int>> &dp)
 // // //     {
-// // //         if(row==0 && col==0) return 1;
+// // //         if(row==0 && col==0) return dp[row][col]=1;
 // // //         if(row<0 || col<0) return 0;
-// // //         return f(row-1, col)+f(row, col-1);
+// // //         if(dp[row][col]!=-1) return dp[row][col];
+// // //         return dp[row][col]=f(row-1, col, dp)+f(row, col-1, dp);
 // // //     }
 // // //     int uniquePaths(int m, int n) {
-// // //         return f(m-1, n-1);
+// // //         vector<vector<int>> dp(m, vector<int>(n, -1));
+// // //         f(m-1, n-1, dp);
+// // //         return dp[m-1][n-1];
 // // //     }
 // // // };
-// // // =========================MEMOIZATION
+// // // ===========================TABULATION
 // // class Solution {
 // // public:
-// //     int f(int row, int col, vector<vector<int>> &dp)
-// //     {
-// //         if(row==0 && col==0) return dp[row][col]=1;
-// //         if(row<0 || col<0) return 0;
-// //         if(dp[row][col]!=-1) return dp[row][col];
-// //         return dp[row][col]=f(row-1, col, dp)+f(row, col-1, dp);
-// //     }
 // //     int uniquePaths(int m, int n) {
-// //         vector<vector<int>> dp(m, vector<int>(n, -1));
-// //         f(m-1, n-1, dp);
+// //         vector<vector<int>> dp(m, vector<int>(n));
+// //         for(int i=0; i<m; i++)
+// //         {
+// //             for(int j=0; j<n; j++)
+// //             {
+// //                 if(i==0 && j==0) dp[i][j]=1;
+// //                 else
+// //                 {
+// //                     int left=0, up=0;
+// //                     if(i>0) up=dp[i-1][j];
+// //                     if(j>0) left=dp[i][j-1];
+// //                     dp[i][j]=up+left;
+// //                 }
+// //             }
+// //         }
 // //         return dp[m-1][n-1];
 // //     }
 // // };
-// // ===========================TABULATION
+// // ================================SPACE OPTIMIZATION
 // class Solution {
 // public:
 //     int uniquePaths(int m, int n) {
-//         vector<vector<int>> dp(m, vector<int>(n));
+//         vector<int> prev(n, 0);
 //         for(int i=0; i<m; i++)
 //         {
+//             vector<int> temp(n, 0);
 //             for(int j=0; j<n; j++)
 //             {
-//                 if(i==0 && j==0) dp[i][j]=1;
-//                 else
+//                 if(i==0 && j==0) 
 //                 {
-//                     int left=0, up=0;
-//                     if(i>0) up=dp[i-1][j];
-//                     if(j>0) left=dp[i][j-1];
-//                     dp[i][j]=up+left;
+//                     temp[j]=1;
+//                     continue;
 //                 }
+//                 int left=0, up=0;
+//                 if(i>0) up=prev[j];
+//                 if(j>0) left=temp[j-1];
+//                 temp[j]=left+up;
 //             }
+//             prev=temp;
 //         }
-//         return dp[m-1][n-1];
+//         return prev[n-1];
 //     }
 // };
-// ================================SPACE OPTIMIZATION
+// ======================================COMBINATORICS APPROCH
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<int> prev(n, 0);
-        for(int i=0; i<m; i++)
-        {
-            vector<int> temp(n, 0);
-            for(int j=0; j<n; j++)
-            {
-                if(i==0 && j==0) 
-                {
-                    temp[j]=1;
-                    continue;
-                }
-                int left=0, up=0;
-                if(i>0) up=prev[j];
-                if(j>0) left=temp[j-1];
-                temp[j]=left+up;
-            }
-            prev=temp;
-        }
-        return prev[n-1];
+    int uniquePaths(int m, int n)
+    {
+//         total unique path = NCr
+//         N = (m-1) + (n-1) = m+n-2 (Total no. of directions)
+//         r = m-1 or r = n-1
+        int N=m+n-2;
+        int r=m-1;
+        double res=1;
+        // method to calculate nCr in O(n);
+        for(int i=1; i<=r; i++)
+            res=res*(N-r+i)/i;
+        return (int)res;
     }
 };
