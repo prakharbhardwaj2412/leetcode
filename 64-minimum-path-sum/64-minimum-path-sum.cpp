58cd615989ea37e21de6c56075b0b6ed0e4df224
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-10-05 11:54:11 +0530

    Time: 12 ms (77.64%), Space: 10.1 MB (56.70%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,30 +1,45 @@
+// // ==========================TABULATION
+// class Solution {
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int n=grid.size();
+//         int m=grid[0].size();
+//         vector<vector<int>> dp(n, vector<int>(m, 0));
+//         for(int i=0; i<n; i++) {
+//             for(int j=0; j<m; j++) {
+//                 if(i==0 && j==0) dp[i][j]=grid[i][j];
+//                 else {
+//                     int left=INT_MAX, up=INT_MAX;
+//                     if(i>0) left=dp[i-1][j];
+//                     if(j>0) up=dp[i][j-1];
+//                     dp[i][j]=grid[i][j]+min(left, up);
+//                 }
+//             }
+//         }
+//         return dp[n-1][m-1];
+//     }
+// };
+
+// =========================SPACE OPTIMIZATION
 class Solution {
 public:
-    // int f() {
-    //     if(i==0 && j==0) return grid[0][0];
-    //     if(i<0 || j<0) return 0;
-    //     int up=f(i-1, j, sum);
-    //     int 
-    // }
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, 0));
+        vector<int> prev(m);
         for(int i=0; i<n; i++) {
+            vector<int> temp(m);
             for(int j=0; j<m; j++) {
-                if(i==0 && j==0) dp[i][j]=grid[i][j];
+                if(i==0 && j==0) temp[j]=grid[i][j];
                 else {
                     int left=INT_MAX, up=INT_MAX;
-                    if(i>0) left=dp[i-1][j];
-                    if(j>0) up=dp[i][j-1];
-                    dp[i][j]=grid[i][j]+min(left, up);
+                    if(i>0) up=prev[j];
+                    if(j>0) left=temp[j-1];
+                    temp[j]=grid[i][j]+min(left, up);
                 }
             }
+            prev=temp;
         }
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) cout<<dp[i][j]<<" ";
-            cout<<endl;
-        }
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
