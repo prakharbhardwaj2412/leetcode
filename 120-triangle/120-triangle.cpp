86287c601165403f837724b7a89744164e8ab91c
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-10-06 21:35:51 +0530

    Time: 130 ms (5.75%), Space: 9.1 MB (10.48%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -14,21 +14,56 @@
 // };
 
 // =============================MEMOIZATION
+// class Solution {
+// public:
+//     int f(int row, int col, vector<vector<int>> &triangle, vector<vector<int>> &dp)
+//     {
+//         if(row>=triangle.size()) return 0;
+//         if(dp[row][col]!=-1) return dp[row][col];
+//         int left=f(row+1, col, triangle, dp);
+//         int right=f(row+1, col+1, triangle, dp);
+//         return dp[row][col]=min(left, right)+triangle[row][col];
+//     }
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n=triangle.size();
+//         int m=triangle[n-1].size();
+//         vector<vector<int>> dp(n, vector<int>(m, -1));
+//         f(0, 0, triangle, dp);
+//         return dp[0][0];
+//     }
+// };
+
+// =============================TABULATION
 class Solution {
 public:
-    int f(int row, int col, vector<vector<int>> &triangle, vector<vector<int>> &dp)
-    {
-        if(row>=triangle.size()) return 0;
-        if(dp[row][col]!=-1) return dp[row][col];
-        int left=f(row+1, col, triangle, dp);
-        int right=f(row+1, col+1, triangle, dp);
-        return dp[row][col]=min(left, right)+triangle[row][col];
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         int m=triangle[n-1].size();
         vector<vector<int>> dp(n, vector<int>(m, -1));
-        f(0, 0, triangle, dp);
-        return dp[0][0];
+        dp[0][0]=triangle[0][0];
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<triangle[i].size(); j++) {
+                int left=INT_MAX, right=INT_MAX;
+                if(j==0) {
+                    left=INT_MAX;
+                    right=dp[i-1][j];
+                }
+                else if(j==triangle[i-1].size())
+                {
+                    left=dp[i-1][j-1];
+                    right=INT_MAX;
+                } else {
+                    left=dp[i-1][j-1];
+                    right=dp[i-1][j];
+                }
+                dp[i][j]=min(left, right)+triangle[i][j];
+                cout<<"("<<left<<" "<<right<<" "<<triangle[i][j]<<" "<<dp[i][j]<<") ";
+            }
+            cout<<endl;
+        }
+        int mini=INT_MAX;
+        for(int i=0; i<m; i++)
+            if(mini>dp[n-1][i]) mini=dp[n-1][i];
+        return mini;
     }
 };
