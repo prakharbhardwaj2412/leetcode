e170494d2fcb6a81e729933a0f5e42e95c399be4
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-10-07 00:43:14 +0530

    Time: 3 ms (98.71%), Space: 9 MB (52.56%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -33,22 +33,44 @@
 //     }
 // };
 
-// =============================TABULATION
+// // =============================TABULATION
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n=triangle.size();
+//         int m=triangle[n-1].size();
+//         vector<vector<int>> dp(n, vector<int>(m, -1));
+//         for(int j=0; j<m; j++)
+//             dp[n-1][j]=triangle[n-1][j];
+//         for(int i=n-2; i>=0; i--) {
+//             for(int j=i; j>=0; j--) {
+//                 int down=triangle[i][j] + dp[i+1][j];
+//                 int diagonal=triangle[i][j] + dp[i+1][j+1];
+//                 dp[i][j]=min(down, diagonal);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
+
+// =============================SPACE OPTIMIZATION
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         int m=triangle[n-1].size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        vector<int> dp(m);
         for(int j=0; j<m; j++)
-            dp[n-1][j]=triangle[n-1][j];
+            dp[j]=triangle[n-1][j];
         for(int i=n-2; i>=0; i--) {
+            vector<int> temp(m);
             for(int j=i; j>=0; j--) {
-                int down=triangle[i][j] + dp[i+1][j];
-                int diagonal=triangle[i][j] + dp[i+1][j+1];
-                dp[i][j]=min(down, diagonal);
+                int down=triangle[i][j] + dp[j];
+                int diagonal=triangle[i][j] + dp[j+1];
+                temp[j]=min(down, diagonal);
             }
+            dp=temp;
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
