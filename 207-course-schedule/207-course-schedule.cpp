dcfc013e3d06237417e88bc67bda58771b4abecf
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-10-07 23:58:30 +0530

    Time: 27 ms (81.56%), Space: 13.5 MB (77.09%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    bool detectCycleDFS(int node, vector<int> adjLi[], vector<int> &vis, vector<int> &pathVis) {
-        vis[node]=1;
-        pathVis[node]=1;
+    bool detectCycleDFS(int node, vector<int> adjLi[], vector<int> &vis) {
+        vis[node]=2;
         // travel for adjacent nodes
         for(auto adjNode:adjLi[node]) {
             // when node is not visited
-            if(!vis[adjNode]) {
-                if(detectCycleDFS(adjNode, adjLi, vis, pathVis) == true) return true;
+            if(vis[adjNode]==0) {
+                if(detectCycleDFS(adjNode, adjLi, vis) == true) return true;
             }
             // if node has been previously visited
             // but it has to be visited on the same path
-            else if(pathVis[adjNode]) return true;
+            else if(vis[adjNode]==2) return true;
         }
-        pathVis[node]=0;
+        vis[node]=1;
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
@@ -22,10 +21,10 @@ public:
             adjLi[prerequisites[i][0]].push_back(prerequisites[i][1]);
         
         vector<int> vis(numCourses, 0);
-        vector<int> pathVis(numCourses, 0);
+        // vector<int> pathVis(numCourses, 0);
         for(int i=0; i<numCourses; i++) {
-            if(!vis[i])
-                if(detectCycleDFS(i, adjLi, vis, pathVis)) return false;
+            if(vis[i]==0)
+                if(detectCycleDFS(i, adjLi, vis)) return false;
         }
         return true;
     }
