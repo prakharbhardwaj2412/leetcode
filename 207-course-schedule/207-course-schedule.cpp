7b79ed0068ae0a6011cb27ef48504935cfe5a056
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-10-09 14:58:52 +0530

    Time: 25 ms (87.42%), Space: 13.1 MB (92.63%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -16,18 +16,43 @@ public:
         pathVis[node]=0;
         return false;
     }
-    // bool detectCycleBFS()
+    bool detectCycleBFS(int V, vector<int> adjLi[]) {
+        vector<int> indegree(V, 0);
+        for(int i=0; i<V; i++) {
+            for(int it:adjLi[i]) 
+                indegree[it]++;
+        }
+        queue<int> q;
+        for(int i=0; i<V; i++) {
+            if(indegree[i]==0) q.push(i);
+        }
+        int countNode=0;
+        while(!q.empty()) {
+            int node=q.front();
+            q.pop();
+            countNode++;
+            for(int it:adjLi[node]) {
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        if(countNode==V) return false;
+        return true;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> adjLi[numCourses];
         for(int i=0; i<prerequisites.size(); i++)
             adjLi[prerequisites[i][0]].push_back(prerequisites[i][1]);
         
-        vector<int> vis(numCourses, 0);
-        vector<int> pathVis(numCourses, 0);
-        for(int i=0; i<numCourses; i++) {
-            if(vis[i]==0)
-                if(detectCycleDFS(i, adjLi, vis, pathVis)) return false;
-        }
-        return true;
+//         DFS function to detect cycle in a graph
+        // vector<int> vis(numCourses, 0);
+        // vector<int> pathVis(numCourses, 0);
+        // for(int i=0; i<numCourses; i++) {
+        //     if(vis[i]==0)
+        //         if(detectCycleDFS(i, adjLi, vis, pathVis)) return false;
+        // }
+        // return true;
+//         BFS function to detect cycle in a graph
+        return !detectCycleBFS(numCourses, adjLi);
     }
 };
