fab2b9fad0c950f2f61e707cba399e68497352d4
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-11-07 23:44:08 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (17.66%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        string s = to_string(num);
-        for(int i=0; i<s.size(); i++) {
-            if(s[i]=='6') {
-                s[i]='9';
-                break;
+        // string numString = to_string(num);
+        // for(auto &currChar : numString) {
+        //     if(currChar=='6') {
+        //         currChar='9';
+        //         break;
+        //     }
+        // }
+        // return stoi(numString);
+        
+        int numCopy = num;
+        int indexFirstSix=-1;
+        int currDigit=0;
+        
+        while(numCopy>0) {
+            if(numCopy % 10 == 6) {
+                indexFirstSix = currDigit;
             }
+            numCopy /= 10;
+            currDigit++;
         }
-        return stoi(s);
+        
+        return indexFirstSix == -1 ? num : num + 3 * pow(10, indexFirstSix);
         
     }
 };
