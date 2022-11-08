ccc808f1059993cfe7b9df56ed3fce952616cf47
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-11-08 18:06:51 +0530

    Time: 0 ms (100.00%), Space: 6 MB (97.69%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     void check(int i, string &s) {
-        cout<<i<<" "<<s<<" "<<s.size()<<endl;
         if(i >= s.size()) return;
         if(abs(s[i] - s[i+1]) == 32) {
             s.erase(s.begin() + i + 1);
             s.erase(s.begin() + i);
             check(0, s);
         } else if(i<s.size()-1) 
-        {
-            cout<<"hello"<<endl;
             check(i+1, s);
-        }
     }
     string makeGood(string s) {
         check(0, s);
