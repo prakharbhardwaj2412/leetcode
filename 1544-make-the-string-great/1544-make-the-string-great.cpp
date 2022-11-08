160338dd46f5381c71a40c171473afa9ae8670ea
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-11-08 18:31:54 +0530

    Time: 7 ms (40.60%), Space: 6.9 MB (24.11%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    void check(int i, string &s) {
-        if(i >= s.size()) return;
-        if(abs(s[i] - s[i+1]) == 32) {
-            s.erase(s.begin() + i + 1);
-            s.erase(s.begin() + i);
-            check(0, s);
-        } else 
-            check(i+1, s);
-    }
     string makeGood(string s) {
-        // check(0, s);
-        // return s;
+        // RECURSIVE
+        for(int i=0; i<s.size(); i++) {
+            if(abs(s[i] - s[i+1]) == 32) {
+                s.erase(s.begin() + i + 1);
+                s.erase(s.begin() + i);
+                return makeGood(s);
+            }
+        }
+        return s;
+        // ITTERATIVE
         int i=0;
         while(s.size() > 1 && i < s.size()) {
             if(abs(s[i] - s[i+1]) == 32) {
