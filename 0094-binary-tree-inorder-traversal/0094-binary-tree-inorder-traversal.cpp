a76eddfabb4825c46239627a25104aa46e0a7c40
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-11-09 01:11:08 +0530

    Time: 3 ms (63.44%), Space: 8.5 MB (15.69%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+//     vector<int> res;
+//     vector<int> inorderTraversal(TreeNode* root) {
+// //         RECURSIVE INORDER
+//         // if(root)
+//         // {
+//         //     inorderTraversal(root->left);
+//         //     res.push_back(root->val);
+//         //     inorderTraversal(root->right);
+//         // }
+//         // return res;
+// //         ITERATIVE INORDER=======================
+//         stack<TreeNode *> st;
+//         TreeNode* t=root;
+//         while(t || !st.empty())
+//         {
+//             if(t)
+//             {
+//                 st.push(t);
+//                 t=t->left;
+//             }
+//             else
+//             {
+//                 t=st.top();
+//                 st.pop();
+//                 res.push_back(t->val);
+//                 t=t->right;
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<pair<TreeNode*, int>> st;
+        st.push({root, 1});
+        vector<int> pre, in, post;
+        if(root==NULL) return {};
+        while(!st.empty()) {
+            auto it = st.top();
+            st.pop();
+            if(it.second == 1) {
+                pre.push_back(it.first->val);
+                it.second++;
+                st.push(it);
+                if(it.first->left) st.push({it.first->left, 1});
+            }
+            else if(it.second == 2) {
+                in.push_back(it.first->val);
+                it.second++;
+                st.push(it);
+                if(it.first->right) st.push({it.first->right, 1});
+            }
+            else 
+                post.push_back(it.first->val);
+        }
+        return in;
+
+    }
+};
