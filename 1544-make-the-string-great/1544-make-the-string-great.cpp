190f9a9db3fd346672ce0b4e623bced16721b0cf
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-11-08 18:27:33 +0530

    Time: 5 ms (54.29%), Space: 6.2 MB (82.16%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -10,7 +10,18 @@ public:
             check(i+1, s);
     }
     string makeGood(string s) {
-        check(0, s);
+        // check(0, s);
+        // return s;
+        int i=0;
+        while(s.size() > 1 && i < s.size()) {
+            if(abs(s[i] - s[i+1]) == 32) {
+                s.erase(s.begin() + i + 1);
+                s.erase(s.begin() + i);
+                i = 0;
+            } else {
+                i++;
+            }
+        }
         return s;
     }
 };
