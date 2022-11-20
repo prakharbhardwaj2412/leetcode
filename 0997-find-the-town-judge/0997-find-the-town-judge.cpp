8c57b811792453100e3a7648395ec5350fd1d769
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-11-20 08:56:58 +0530

    Time: 384 ms (56.46%), Space: 60.8 MB (82.42%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-//     void dfs(int ind, vector<int> &vis, vector<int> &pathVis, vector<int> adj[]) {
-//         vis[ind] = 1;
-//         pathVis[ind] = 1;
-        
-//         pathVis[ind] = 0;
-//     }
     int findJudge(int n, vector<vector<int>>& trust) {
-        set<int> adj[n + 1];
-        for(int i = 0; i < trust.size(); i++) {
-            adj[trust[i][0]].insert(trust[i][1]);
-        } 
+//         Intuition:
+    //         Consider trust as a graph, all pairs are directed edge.
+    //         The point with in-degree - out-degree = N - 1 become the judge.
+
+//         Explanation:
+    //         Count the degree, and check at the end.
+
+//         Time Complexity:
+    //         Time O(T + N), space O(N)
+        vector<int> count(n + 1, 0); 
         
-        for(int i = 1; i< n + 1; i++) {
-            adj[i].insert(0);
+        for (auto& t : trust) {
+            count[t[0]]--; count[t[1]]++;
         }
         
-//         vector<int> vis(n, 0);
-//         vector<int> pathVis(n, 0);
+        for(int i = 1; i <= n; i++) if(count[i] == n - 1) return i;
         
-//         for(int i = 0; i < n; i++) {
-//             if(!vis[i]) dfs(i, vis, pathVis, adj);
-//         }
-        int judge = -1;
-        for(int i = 0; i < n + 1; i++) {
-            if(adj[i].size() == 1 &&  adj[i].find(0) != adj[i].end()) {
-                judge =  i;
-                break;
-            }
-        }
-        cout<<judge<<endl;
-        for(int i = 1; i< n + 1; i++) if(i!=judge && adj[i].find(judge) == adj[i].end()) return -1;
-        
-        return judge;
+        return -1;
+
     }
 };
