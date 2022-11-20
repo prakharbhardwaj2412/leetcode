0822fc9c89d2a475d39426da2b2d5d316de0f7b3
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-11-20 22:40:16 +0530

    Time: 21 ms (83.42%), Space: 14.3 MB (24.80%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,40 +7,56 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// BFS TRAVERSAL
 class Solution {
 public:
-    bool getPath (TreeNode* root, TreeNode* node, vector<TreeNode*> &arr) {
-        if(!root) return false;
-        arr.push_back(root);
-        if(root->val == node->val) return true;
-        
-        
-        
-        if(getPath(root->left, node, arr) || getPath(root->right, node, arr)) return true;
-        
-        arr.pop_back();
-        
-        return false;
-    } 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> arr1;
-        vector<TreeNode*> arr2;
+        if(root == NULL || root == p || root == q) return root;
         
-        getPath(root, p, arr1);
-        getPath(root, q, arr2);
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
         
-        TreeNode* tmp = root;
-        
-        for(auto it : arr1) cout<<it->val<<" ";
-        cout<<endl;
-        for(auto it : arr2) cout<<it->val<<" ";
-        
-        for(int i = 0; i < min(arr1.size(), arr2.size()); i++) {
-            // cout<<arr1[i]->val<<" "<<arr2[i]->val<<endl;
-            if(arr1[i] == arr2[i]) tmp = arr1[i];
-        }
-        
-        return tmp;
+        if(left == NULL) return right;
+        else if(right == NULL) return left;
+        else return root;
         
     }
 };
+// BRUTE FORCE APPROACH
+// class Solution {
+// public:
+//     bool getPath (TreeNode* root, TreeNode* node, vector<TreeNode*> &arr) {
+//         if(!root) return false;
+//         arr.push_back(root);
+//         if(root->val == node->val) return true;
+        
+        
+        
+//         if(getPath(root->left, node, arr) || getPath(root->right, node, arr)) return true;
+        
+//         arr.pop_back();
+        
+//         return false;
+//     } 
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         vector<TreeNode*> arr1;
+//         vector<TreeNode*> arr2;
+        
+//         getPath(root, p, arr1);
+//         getPath(root, q, arr2);
+        
+//         TreeNode* tmp = root;
+        
+//         for(auto it : arr1) cout<<it->val<<" ";
+//         cout<<endl;
+//         for(auto it : arr2) cout<<it->val<<" ";
+        
+//         for(int i = 0; i < min(arr1.size(), arr2.size()); i++) {
+//             // cout<<arr1[i]->val<<" "<<arr2[i]->val<<endl;
+//             if(arr1[i] == arr2[i]) tmp = arr1[i];
+//         }
+        
+//         return tmp;
+        
+//     }
+// };
