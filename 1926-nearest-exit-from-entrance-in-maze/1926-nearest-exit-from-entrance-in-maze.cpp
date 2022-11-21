e8d6404eee07501e0cf85ae513eb05fc3126b7e5
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-11-21 19:10:51 +0530

    Time: 123 ms (97.04%), Space: 30.5 MB (61.76%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         int m = maze.size(), n = maze[0].size();
-        queue<pair<pair<int, int>, int>> q;
+        
+        // Start BFS from the entrance, and use a queue `queue` to store all 
+        // the cells to be visited.
+        queue<pair<pair<int, int>, int>> q; // {{row, col}, exit_count}
         q.push({{entrance[0], entrance[1]}, 0});
         
         // Mark the entrance as visited since its not a exit.
@@ -18,19 +21,25 @@ public:
             int col = it.first.second;
             int exit = it.second;
             
-            
+            // For the current cell, check its four neighbor cells.
             for(int i = 0; i < 4; i++) {
                 int nrow = row + delrow[i];
                 int ncol = col + delcol[i];
                 
+                // If there exists an unvisited empty neighbor:
                 if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && maze[nrow][ncol] != '+') {
+                    
+                    // If this empty cell is an exit, return distance + 1.
                     if(nrow == 0 || nrow == m - 1 || ncol == 0 || ncol == n - 1) return exit + 1;
+                    
+                    // Otherwise, add this cell to 'queue' and mark it as visited.
                     maze[nrow][ncol] = '+';
                     q.push({{nrow, ncol}, exit + 1});
                 }
             }
             
         }
+        // If we finish iterating without finding an exit, return -1.
         return -1;
     }
 };
