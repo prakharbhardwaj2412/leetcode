8050f95f52374c4e8b6851080a97e7185c31b843
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-11-24 09:41:01 +0530

    Time: 28 ms (75.79%), Space: 26.4 MB (28.01%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,38 +9,74 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// O(n) solution
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        if(preorder.size() == 0) return nullptr;
-        TreeNode *newNode = new TreeNode(preorder[0]);
-        int i = 0;
-        for(i = 0; i < inorder.size(); i++) if(inorder[i] == preorder[0]) break;
+        // if(inorder.size() != preorder.size()) return nullptr;
         
-        vector<int> leftIn;
-        vector<int> rightIn;
+        int preStart = 0, preEnd = preorder.size() - 1;
+        int inStart = 0, inEnd = inorder.size() - 1;
         
-        for(int j = 0; j < inorder.size(); j++) {
-            if(j < i) leftIn.push_back(inorder[j]);
-            if(j > i) rightIn.push_back(inorder[j]);
-        }
+        map<int, int> inMap;
+        for(int i = 0; i < inorder.size(); i++) inMap[inorder[i]] = i;
         
-        vector<int> leftPre;
-        vector<int> rightPre;
+        return buildTreeInPre(inorder, inStart, inEnd,
+                             preorder, preStart, preEnd, inMap);
+    }
+    
+    TreeNode* buildTreeInPre(vector<int>& inorder, int inStart, int inEnd, vector<int>& preorder, int preStart, int preEnd, map<int, int>& inMap) {
         
-        for(int j = 1; j < preorder.size(); j++) {
-            if(i > 0) {
-                leftPre.push_back(preorder[j]);
-                i--;
-            }
-            else {
-                rightPre.push_back(preorder[j]);
-            }
-        }
+        if(inStart > inEnd || preStart > preEnd) return nullptr;
         
-        newNode->left = buildTree(leftPre, leftIn);
-        newNode->right = buildTree(rightPre, rightIn);
+        TreeNode* root = new TreeNode(preorder[preStart]); 
         
-        return newNode;
+        int inRoot = inMap[preorder[preStart]];
+        
+        int numsLeft = inRoot - inStart;
+        
+        root->left = buildTreeInPre(inorder, inStart, inRoot - 1,
+                             preorder, preStart + 1, preStart + numsLeft, inMap);
+        root->right = buildTreeInPre(inorder, inRoot + 1, inEnd,
+                             preorder, preStart + numsLeft + 1, preEnd, inMap);
+        
+        return root;
     }
 };
+
+
+// class Solution {
+// public:
+//     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+//         if(preorder.size() == 0) return nullptr;
+//         TreeNode *newNode = new TreeNode(preorder[0]);
+//         int i = 0;
+//         for(i = 0; i < inorder.size(); i++) if(inorder[i] == preorder[0]) break;
+        
+//         vector<int> leftIn;
+//         vector<int> rightIn;
+        
+//         for(int j = 0; j < inorder.size(); j++) {
+//             if(j < i) leftIn.push_back(inorder[j]);
+//             if(j > i) rightIn.push_back(inorder[j]);
+//         }
+        
+//         vector<int> leftPre;
+//         vector<int> rightPre;
+        
+//         for(int j = 1; j < preorder.size(); j++) {
+//             if(i > 0) {
+//                 leftPre.push_back(preorder[j]);
+//                 i--;
+//             }
+//             else {
+//                 rightPre.push_back(preorder[j]);
+//             }
+//         }
+        
+//         newNode->left = buildTree(leftPre, leftIn);
+//         newNode->right = buildTree(rightPre, rightIn);
+        
+//         return newNode;
+//     }
+// };
