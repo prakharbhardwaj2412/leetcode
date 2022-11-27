ea00e32ece394c52f084db4b1695c9ca674258c8
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-11-27 10:03:58 +0530

    Time: 12 ms (50.10%), Space: 12.7 MB (77.80%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -26,29 +26,40 @@
 // };
 
 // 2) ITERATIVE APPROACH
-class Solution {
-public:
-    void flatten(TreeNode* root) {
-        if(!root) return;
-        
-        stack<TreeNode*> st;
-        st.push(root);
-        
-        while(!st.empty()) {
-            TreeNode* curr = st.top(); st.pop();
-            if(curr->right) st.push(curr->right);
-            if(curr->left) st.push(curr->left);
-            
-            if(!st.empty()) curr->right = st.top();
-            curr->left = NULL;
-        }
-    }
-};
-
-// // 3) MORRIS TRAVERSAL APPROACH
 // class Solution {
 // public:
 //     void flatten(TreeNode* root) {
+//         if(!root) return;
         
+//         stack<TreeNode*> st;
+//         st.push(root);
+        
+//         while(!st.empty()) {
+//             TreeNode* curr = st.top(); st.pop();
+//             if(curr->right) st.push(curr->right); // RIGHT
+//             if(curr->left) st.push(curr->left); // LEFT
+            
+//             if(!st.empty()) curr->right = st.top(); // LOGIC
+//             curr->left = NULL;
+//         }
 //     }
 // };
+
+// 3) MORRIS TRAVERSAL APPROACH
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* curr = root;
+        while(curr != NULL) {
+            if(curr->left != NULL) {
+                TreeNode* prev = curr->left;
+                while(prev->right) prev = prev->right;
+                
+                prev->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+            curr = curr->right;
+        }
+    }
+};
