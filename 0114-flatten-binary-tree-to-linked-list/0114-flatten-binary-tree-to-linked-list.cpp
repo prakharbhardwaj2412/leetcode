955000457c0bcc5fa74e984d6c61f6f647d3ad8e
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-11-27 09:20:44 +0530

    Time: 16 ms (25.07%), Space: 12.8 MB (36.75%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,26 +11,39 @@
  */
 // There are three solutions for this:
 // 1) RECURSIVE APPROACH
-class Solution {
-public:
-    TreeNode* prev = nullptr;
-    void flatten(TreeNode* root) {
-        if(!root) return;
-        flatten(root->right);
-        flatten(root->left);
-        root->right = prev;
-        root->left = nullptr;
-        prev = root;
-    }
-};
-
-// // 2) ITERATIVE APPROACH
 // class Solution {
 // public:
+//     TreeNode* prev = nullptr;
 //     void flatten(TreeNode* root) {
-        
+//         // =================REVERSE POSTORDER
+//         if(!root) return;
+//         flatten(root->right); // RIGHT
+//         flatten(root->left); // LEFT
+//         root->right = prev; // LOGIC
+//         root->left = nullptr;
+//         prev = root;
 //     }
 // };
+
+// 2) ITERATIVE APPROACH
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(!root) return;
+        
+        stack<TreeNode*> st;
+        st.push(root);
+        
+        while(!st.empty()) {
+            TreeNode* curr = st.top(); st.pop();
+            if(curr->right) st.push(curr->right);
+            if(curr->left) st.push(curr->left);
+            
+            if(!st.empty()) curr->right = st.top();
+            curr->left = NULL;
+        }
+    }
+};
 
 // // 3) MORRIS TRAVERSAL APPROACH
 // class Solution {
