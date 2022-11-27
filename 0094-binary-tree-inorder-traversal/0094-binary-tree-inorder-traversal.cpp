c14d28ba241f7d83e0458425133c163b04548f59
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-11-27 09:07:48 +0530

    Time: 3 ms (63.21%), Space: 8.5 MB (39.84%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -13,7 +13,7 @@
 // public:
 //     vector<int> res;
 //     vector<int> inorderTraversal(TreeNode* root) {
-// //         RECURSIVE INORDER
+// //         =======================RECURSIVE INORDER
 //         // if(root)
 //         // {
 //         //     inorderTraversal(root->left);
@@ -21,7 +21,7 @@
 //         //     inorderTraversal(root->right);
 //         // }
 //         // return res;
-// //         ITERATIVE INORDER=======================
+// //         =================================ITERATIVE INORDER=======================
 //         stack<TreeNode *> st;
 //         TreeNode* t=root;
 //         while(t || !st.empty())
@@ -43,33 +43,66 @@
 //     }
 // };
 
-// ALL TRAVERSAL IN ONE SHOT
+// ==========================ALL TRAVERSAL IN ONE SHOT
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         stack<pair<TreeNode*, int>> st;
+//         st.push({root, 1});
+//         vector<int> pre, in, post;
+//         if(root==NULL) return {};
+//         while(!st.empty()) {
+//             auto it = st.top();
+//             st.pop();
+//             if(it.second == 1) {
+//                 pre.push_back(it.first->val);
+//                 it.second++;
+//                 st.push(it);
+//                 if(it.first->left) st.push({it.first->left, 1});
+//             }
+//             else if(it.second == 2) {
+//                 in.push_back(it.first->val);
+//                 it.second++;
+//                 st.push(it);
+//                 if(it.first->right) st.push({it.first->right, 1});
+//             }
+//             else 
+//                 post.push_back(it.first->val);
+//         }
+//         return in;
+
+//     }
+// };
+
+// ==========================MORRIS TRAVERSAL
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        stack<pair<TreeNode*, int>> st;
-        st.push({root, 1});
-        vector<int> pre, in, post;
-        if(root==NULL) return {};
-        while(!st.empty()) {
-            auto it = st.top();
-            st.pop();
-            if(it.second == 1) {
-                pre.push_back(it.first->val);
-                it.second++;
-                st.push(it);
-                if(it.first->left) st.push({it.first->left, 1});
+        vector<int> inorder;
+        TreeNode* curr = root;
+        while(curr != NULL) {
+            if(curr->left == NULL) {
+                inorder.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                TreeNode* prev = curr->left;
+                
+                while(prev->right && prev->right != curr) {
+                    prev = prev->right;
+                }
+                
+                if(prev->right == NULL) {
+                    prev->right = curr;
+                    curr = curr->left;
+                } else {
+                    prev->right = NULL;
+                    inorder.push_back(curr->val);
+                    curr = curr->right;
+                }
             }
-            else if(it.second == 2) {
-                in.push_back(it.first->val);
-                it.second++;
-                st.push(it);
-                if(it.first->right) st.push({it.first->right, 1});
-            }
-            else 
-                post.push_back(it.first->val);
         }
-        return in;
+        
+        return inorder;
 
     }
 };
