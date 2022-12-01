968b6f2efc90677a696e77ba18750df2b23d1fa8
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-12-02 01:27:30 +0530

    Time: 9 ms (28.20%), Space: 6.6 MB (56.64%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -4,7 +4,8 @@ public:
         int n = s.size();
         int first = 0, second = 0;
         for(int i = 0; i < n; i++) {
-            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||s[i] == 'o' ||s[i] == 'u' ||s[i] == 'A' || s[i] == 'E' ||s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
+            s[i]=tolower(s[i]);
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||s[i] == 'o' ||s[i] == 'u') {
                 if(i < n/2) first++;
                 else second++;
             }
