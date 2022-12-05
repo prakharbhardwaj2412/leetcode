0d1f3c71231b6a9a09d099cb5107918c1e43731f
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-12-05 10:49:47 +0530

    Time: 57 ms (5.12%), Space: 23.9 MB (98.12%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,22 +9,52 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// INORDER TRAVERSAL
+// class Solution {
+// public:
+//     void traversal(TreeNode* root, int k, int &count, int &ans) {
+//         if(!root) return;
+        
+//         traversal(root->left, k, count, ans);
+//         count++;
+//         if(count == k) {
+//             ans = root->val;
+//             return;
+//         }
+//         traversal(root->right, k, count, ans);
+//     }
+//     int kthSmallest(TreeNode* root, int k) {
+//         int count = 0, ans = 0;
+//         traversal(root, k, count, ans);
+//         return ans;
+//     }
+// };
+// MORRI'S TRAVERSAL
 class Solution {
 public:
-    void traversal(TreeNode* root, int k, int &count, int &ans) {
-        if(!root) return;
-        
-        traversal(root->left, k, count, ans);
-        count++;
-        if(count == k) {
-            ans = root->val;
-            return;
-        }
-        traversal(root->right, k, count, ans);
-    }
     int kthSmallest(TreeNode* root, int k) {
         int count = 0, ans = 0;
-        traversal(root, k, count, ans);
+        TreeNode* curr = root;
+        while(curr) {
+            if(!curr->left) {
+                count++;
+                if(count == k) ans = curr->val;
+                curr = curr->right;
+            } else {
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right != curr) prev = prev->right;
+                
+                if(!prev->right) {
+                    prev->right = curr;
+                    curr = curr->left;
+                } else {
+                    prev->right = NULL;
+                    count++;
+                    if(count == k) ans = curr->val;
+                    curr = curr->right;
+                }
+            }
+        }
         return ans;
     }
 };
