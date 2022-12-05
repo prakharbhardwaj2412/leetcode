a4a8a564f1f060daff3ae2c2c057c4568d66e9d4
Prakhar Bhardwaj <prakharbhardwaj23@gmail.com>
2022-12-06 01:23:59 +0530

    Time: 8 ms (60.43%), Space: 13.7 MB (76.07%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -9,40 +9,53 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// ==========================O(n*n) solution==========================
 class Solution {
 public:
-    TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(!root) return new TreeNode(val);
+    TreeNode* bstFromPreorder(vector<int> &pre, int &i, int bound) {
+        if(i == pre.size() || pre[i] > bound) return NULL;
         
-        TreeNode *curr = root;
+        TreeNode* root = new TreeNode(pre[i++]);
+        root->left = bstFromPreorder(pre, i, root->val);
+        root->right = bstFromPreorder(pre, i, bound);
         
-        while(true) {
-            if(curr->val <= val) {
-                if(curr->right) curr = curr->right;
-                else {
-                    curr->right = new TreeNode(val);
-                    break;
-                }
-            } else {
-                if(curr->left) curr = curr->left;
-                else {
-                    curr->left = new TreeNode(val);
-                    break;
-                }
-            }
-        }
         return root;
+        
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        if(preorder.size() == 0) return NULL;
-        
-        
-        TreeNode* root = NULL;
-        
-        for(int i = 0; i < preorder.size(); i++) {
-            root = insertIntoBST(root, preorder[i]);
-        }
-        
-        return root;
+        int i = 0;
+        return bstFromPreorder(preorder, i, INT_MAX);
     }
 };
+// ==========================O(n*n) solution==========================
+// class Solution {
+// public:
+//     TreeNode* insertIntoBST(TreeNode* root, int val) {
+//         if(!root) return new TreeNode(val);
+//         TreeNode *curr = root;
+//         while(true) {
+//             if(curr->val <= val) {
+//                 if(curr->right) curr = curr->right;
+//                 else {
+//                     curr->right = new TreeNode(val);
+//                     break;
+//                 }
+//             } else {
+//                 if(curr->left) curr = curr->left;
+//                 else {
+//                     curr->left = new TreeNode(val);
+//                     break;
+//                 }
+//             }
+//         }
+//         return root;
+//     }
+//     TreeNode* bstFromPreorder(vector<int>& preorder) {
+//         if(preorder.size() == 0) return NULL;
+//         TreeNode* root = NULL;
+//         for(int i = 0; i < preorder.size(); i++) {
+//             root = insertIntoBST(root, preorder[i]);
+//         }
+//         return root;
+//     }
+// };
